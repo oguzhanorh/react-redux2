263ef654ebf9114d2abb44cc28e41e92729b14d8
oguzhan.orh1907@gmail.com <oguzhan.orh1907@gmail.com>
2022-09-11 15:21:43 +0300

    show data in console

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,11 @@
 import React from 'react';
-import { selectSong } from '../actions';
+import SongList from './SongList';
 
 const App = () => {
-  return <div>hello this first app</div>;
+  return (
+    <div>
+      <SongList />
+    </div>
+  );
 };
 export default App;
---
+++ src/components/SongList.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+class SongList extends Component {
+  render() {
+    return <div>SongList</div>;
+  }
+}
+
+const mapStateToProps = (state) => {
+  return { songs: state.songs };
+};
+
+export default connect(mapStateToProps)(SongList);
